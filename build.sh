667ebd282a9f45a9c32d097d9df2564276e4673c
Michael A. Bodkin <michael.bodkin@hitachivantara.com>
2020-04-20 22:21:48 -0400

    Rolled versions.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-export BUILD_VERSION=v0.1.2
+export BUILD_VERSION=v0.1.3
 export DOCKER_REPO=baudekin
 export R_VERSION=r3.6.3
 export SPARKLYR_VERSION=sparklyr1.1.0_dplyr0.8.5
@@ -29,4 +29,6 @@ docker push ${DOCKER_REPO}/spark3.0.0_sparkbuild:${BUILD_VERSION}
 docker build --tag ${DOCKER_REPO}/spark3.0.0_pybuild_${PYTHON_VERSION}:${BUILD_VERSION} ./pybuild3.0
 docker push ${DOCKER_REPO}/spark3.0.0_pybuild_${PYTHON_VERSION}:${BUILD_VERSION} 
 docker build --tag ${DOCKER_REPO}/spark3.0.0_pynotebook_${PYTHON_VERSION}:${BUILD_VERSION} ./pynotebook3.0
-docker push ${DOCKER_REPO}/spark3.0.0_pynotebook_${PYTHON_VERSION}:${BUILD_VERSION} 
+docker push ${DOCKER_REPO}/spark2.4.5_pybuild_3.6${BUILD_VERSION} 
+docker build --tag ${DOCKER_REPO}/spark2.4.5_pybuild_3.6:${BUILD_VERSION} ./pybuild3.6
+docker push ${DOCKER_REPO}/spark2.4.5_pybuild_3.6:${BUILD_VERSION} 
